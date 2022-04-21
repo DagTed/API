ac458d72db5c768f2bee171c518d2aaa0bc0f4f0
Kirubel321 <72298742+Kirubel321@users.noreply.github.com>
2022-04-21 07:19:10 +0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,47 +1,43 @@
 var i = 0;
-var imgc = ['../images/chandler-bing_1.jpg', '../images/monica_1.jpg', '../images/phoebe_3.jpg', '../images/ross_1.jpg', '../images/joey_1.jpg']
-const url ='https://friends-quotes-api.herokuapp.com/quotes';
-
+const url ="https://friends-quotes-api.herokuapp.com/quotes";
+const fimg = document.getElementById('image');
+const fchar = document.getElementById('charname');
+const fquote = document.getElementById('quote');
+var img = ["../images/chandler-bing_1.jpg", "../images/monica_1.jpg", "../images/phoebe_3.jpg", "../images/ross_1.jpg", "../images/joey_1.jpg"]
 function init(){
     fetch(url).then(function (response){
         return response.json();
     }).then(function (data){
-        quote = document.getElementById("quotes");
-        quote.innerHTML = `<div class="col-sm-3">
-        <div class="card shadow-lg bg-body rounded-3 " style="width: 18rem; height: auto;">
-        <img src="${images(data[i].character)}"  class="card-img-top mx-auto d-block" alt="...">
-        <div class="card-body b-bg"><blockquote class="blockquote">
-        <p>"${data[i].quote}"</p>
-        <footer class="blockquote-footer">${data[i].character}<cite title="Source Title"> 
-        Friends TV Show</cite></footer></blockquote></div></div></div>`;   
-        
+        console.log(data);
+        fimg.src = images(data[i].character);
+        fchar.innerHTML = data[i].character;
+        fquote.innerHTML = data[i].quote;
     }).catch(function(error){
         console.error('Something went wrong retrieving the quotes');
         console.error(error);
     });
-
 }
 function images(dimg){
-    if(dimg === "Chandler"){
-        return imgc[0];
+    if(dimg == "Chandler"){
+        return img[0];
     }
-    else if(dimg === "Monica"){
-        return imgc[1];
+    else if(dimg == "Monica"){
+        return img[1];
     }
-    else if(dimg === "Phoebe"){
-        return imgc[2];
+    else if(dimg == "Phoebe"){
+        return img[2];
     }
-    else if(dimg === "Ross"){
-        return imgc[3];
+    else if(dimg == "Ross"){
+        return img[3];
     }
-    else if(dimg === "Joey"){
-        return imgc[4];
+    else if(dimg == "Joey"){
+        return img[4];
     }
-    else if(dimg === "Rachel"){
-        return imgc[5];
+    else if(dimg == "Rachel"){
+        return img[5];
     }
     else{
-        return 0;
+        return img[6];
     }
 }
 
@@ -55,12 +51,13 @@ function next(){
 }
 function previous(){
     i--;
-    if(i<0){
+    if(i<=0){
         i=18;
     }
     init();
     
 }
+ 
  window.onload = function showquotes(){
     init();
     document.getElementById("nextbtn").addEventListener("click", next);
